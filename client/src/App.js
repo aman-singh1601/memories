8a98f77dde401475bf08b8fa8cda6c14144eedb8
Aman <aman16singh2001@gmail.com>
2023-06-30 17:21:35 +0530

    styled the home header and setting up of api folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import './App.css'
+
+
 
 function App() {
+
   return (
-    <div>App</div>
+    <div >
+      <nav className='home_header'>
+        <h2 className='header_heading'>Memories</h2>
+        <img className='header_image'  src='https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI'
+         alt='' />
+      </nav>
+      <div>  
+        <div >
+            <div >
+                <Posts/>
+            </div>
+            <div >
+                <Form/>
+            </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './Post/Post'
+
+const Posts=()=> {
+  return (
+    <>
+    <h3>Posts</h3>
+    <Post/>
+    <Post/>
+    
+    </>
+  )
+}
+
+export default Posts
