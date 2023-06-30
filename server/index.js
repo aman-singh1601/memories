53d4b71a57272dd25b0aeb63e1f0a02b3f16f654
Aman <aman16singh2001@gmail.com>
2023-06-30 14:53:13 +0530

    setting up og mongoDB

--- server/index.js
+++ server/index.js
@@ -2,3 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app=express();
+
+app.use(bodyParser.json({limit:'300mb' ,extended:true}));
+app.use(bodyParser.urlencoded({limit:'300mb' ,extended:true}));
+app.use(cors());
+
+
+const CONNECTION_URL='mongodb+srv://aman_1601:12aman34@cluster0.drnd5qe.mongodb.net/';
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(()=>{
+        app.listen(PORT,()=> console.log(`Server running on ${PORT}`))
+    })
+    .catch((error)=>{
+        console.log(error.message)
+    })
