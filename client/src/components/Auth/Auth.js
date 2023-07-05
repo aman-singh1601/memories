e196398591d1dea84a6c1de60439066ef33faf79
Aman <aman16singh2001@gmail.com>
2023-07-05 18:39:04 +0530

    created the sign up/in form

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,9 +1,93 @@
 import React from 'react'
 import './Auth.css'
+import {LockOutlined} from '@mui/icons-material';
 
 function Auth() {
+    const isSignUp=false;
+    const handleSubmit=()=>{
+        
+    }
+    const handleChange=()=>{
+
+    }
   return (
-    <div>Auth</div>
+    <div className='auth_container'>
+        <div className='auth_upperContainer'>
+        
+        <form className='auth_form' onSubmit={handleSubmit}>
+        <span className='text_center'>
+            {
+                isSignUp ? "Sign Up" : "Sign In"
+            }
+        </span>
+            {
+                isSignUp && (
+                    <>
+                    <div className='input-container'>
+                    <input
+                      name='firstName'
+                      onChange={handleChange}
+                        required
+                        autoFocus
+                        type='text'
+                    /> 
+                    <label>First Name</label>
+                    </div>
+                    <div className='input-container'>
+                    <input
+                      name='lastName'
+                      onChange={handleChange}
+                        required
+                        autoFocus
+                        type='text'
+                    />
+                    <label>Last Name</label>
+                    </div> 
+                    </>
+                )
+            }
+            <div className='input-container'>
+             <input
+                 name='email'
+                 onChange={handleChange}
+                 required
+                 autoFocus
+                 type='email'
+             /> 
+             <label>Email</label>
+             </div>
+             <div className='input-container'>
+             <input
+                 name='password'
+                 onChange={handleChange}
+                 required
+                 autoFocus
+                 type='password'
+             /> 
+             <label>Password</label>
+             </div>
+             {
+                isSignUp && (
+                    <div className='input-container'>
+                    <input
+                        name='password'
+                        onChange={handleChange}
+                        required
+                        autoFocus
+                        type='password'
+                    /> 
+                    <label>Confirm Password</label>
+                    </div>
+                )
+             }
+               <button type='submit' className='btn' >
+                {isSignUp ? 'Sign Up':'Sign In'}
+               </button>
+        </form>
+        
+        </div>
+
+    </div>
   )
 }
 
